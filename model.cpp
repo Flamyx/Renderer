4800f212fe21d9bb88747a17af473518bd336f30
Peter <petevoro@gmail.com>
2022-10-18 00:40:37 +0300

    wireframe renderer

---
+++ model.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include "model.h"
+
+Model::Model(const char *filename) : verts_(), faces_() {
+    std::ifstream in;
+    in.open (filename, std::ifstream::in);
+    if (in.fail()) return;
+    std::string line;
+    while (!in.eof()) {
+        std::getline(in, line);
+        std::istringstream iss(line.c_str());
+        char trash;
+        if (!line.compare(0, 2, "v ")) {
+            //the line from .obj file is vertice coordinates
+            iss >> trash;
+            Vec3f v;
+            for (int i=0;i<3;i++) iss >> v.raw[i];
+            verts_.push_back(v);
+        } else if (!line.compare(0, 2, "f ")) {
+            //the line from .obj encodes face (triangle) with 3 vertice's indexes
+            std::vector<int> f;
+            int itrash, idx;
+            iss >> trash;
+            //for some reason f v_1/rand/rand v2_idx/rand/rand v3_idx/rand/rand
+            while (iss >> idx >> trash >> itrash >> trash >> itrash) {
+                idx--; // in wavefront obj all indices start at 1, not zero
+                f.push_back(idx);
+            }
+            faces_.push_back(f);
+        }
+    }
+    std::cerr << "# v# " << verts_.size() << " f# "  << faces_.size() << std::endl;
+}
+
+Model::~Model() {
+}
+
+int Model::nverts() {
+    return (int)verts_.size();
+}
+
+int Model::nfaces() {
+    return (int)faces_.size();
+}
+
+std::vector<int> Model::face(int idx) {
+    return faces_[idx];
+}
+
+Vec3f Model::vert(int i) {
+    return verts_[i];
+}
